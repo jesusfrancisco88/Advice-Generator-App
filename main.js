0f1898e9f1d72bb78adf3c78079a1690de3ab003
Jesus Francisco Serna Monsivais <sernafrancisco850@gmail.com>
2023-07-19 15:55:04 -0600

    Inicio de Repositorio

---
+++ main.js
@@ -0,0 +1,19 @@
+import './style.css'
+import viteLogo from '/vite.svg'
+
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <h1 id='app-title'>ADVICE #</h1>
+
+    <div class="card">
+
+
+    </div>
+
+  </div>
+`
+
