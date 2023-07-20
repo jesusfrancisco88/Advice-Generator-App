93a42814a097e6b012b7a87e4db2c722b92356cd
Jesus Francisco Serna Monsivais <sernafrancisco850@gmail.com>
2023-07-20 11:39:40 -0600

    Icono Vite

--- main.js
+++ main.js
@@ -1,18 +1,21 @@
 import { adviceGen } from './src/advice-generator/advice-app.js';
 // import './style.css'
 // import './queries.css'
-import viteLogo from '/vite.svg'
+// import viteLogo from './vite.svg'
 
 document.querySelector('#app').innerHTML = `
   <div>
-  
+    
   <a href="https://vitejs.dev" target="_blank">
-    <img src="${viteLogo}" class="logo" alt="Vite logo" />
+  <img src="./public/vite.svg" class="logo" alt="Vite logo" />
   </a>
-    <div class="card"> </div>
 
+  <div class="card"> </div>
+  
   </div>
-`;
-
+  `;
+  // <a href="https://vitejs.dev" target="_blank">
+  // <img src="${viteLogo}" class="logo" alt="Vite logo" />
+  // </a>
 const element = document.querySelector('.card');
 adviceGen(element);
